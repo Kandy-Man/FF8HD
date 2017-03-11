3cc19661fe4cf26cca72ed92235601e36db6f800
Kandy-Man <Kandy-Man@users.noreply.github.com>
2017-03-11 14:38:39 +0000

    Added Balamb Town and the demo audio tracks.  Balamb town is mostly completed geometry wise, just needs additional work for areas the player could not see in the original game, as well as textures/materials.  Music is unfinished yet usable at the minute.  Could do with better instruments and articulation.  Also boss music is not finshed yet

---
+++ Assets/Scripts/PlayBGM.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBGM : MonoBehaviour
+{
+    [SerializeField]
+    IntroloopAudio BGM;
+
+    void Start()
+    {
+        IntroloopPlayer.Instance.Play(BGM);
+    }
+}
